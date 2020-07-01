30e806c313b772104563d4f050a995bdacfec06f
Helena Rasche <hxr@hx42.org>
2020-07-01 16:11:45 +0200

    init

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+versie=$1
+parsec -g emc workflows get_workflows | \
+	jq '.[] | select(.name | contains("'$versie'")) | [.id, .name] | @tsv' -r | \
+	sed 's/\t/ > "/;s/$/.ga"/;s/^/parsec -g emc workflows export_workflow_dict /;s|/|-|g' | \
+	bash
---
+++ re-upload.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+versie=$1
+for wf in *$versie.ga; do
+	echo Uploading $wf
+	parsec -g emc workflows import_workflow_from_local_path "$wf"
+done
